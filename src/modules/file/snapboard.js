b8061184068675f1b88fe82e706139d4cca4a266
ZFordDev <zforddev@gmail.com>
2026-04-15 22:44:05 +1000

    add SnapBoard integration stub (notes only)

---
+++ src/modules/file/snapboard.js
@@ -0,0 +1,13 @@
+/**
+ * SnapBoard intergration stub
+ * ----------------------------
+ * this file is a stub for the SnapBoard integration, which will allow users to share their Snapboard workspace with SnapDock.
+ * 
+ * The SnapBoard WorkSpace
+ * ------------------------
+ * in SnapBoard you can drag and drop files and folders into the kanban board during the planning phase. 
+ * This allows you to organize your files and folders in a visual way, then export the virtual workspace to SnapDock, which will create a new workspace with the same structure and files.
+ * 
+ * The SnapBoard integration will be implemented in the future, but for now this file serves as a placeholder for the integration and a reference for the future implementation.
+ * 
+ *  */

07c3bf8b03b57372902f7ee3be903b695098151f
ZFordDev <zforddev@gmail.com>
2026-06-12 23:41:54 +1000

    removed from root now in scripts

--- build.js
+++
@@ -1,8 +0,0 @@
-const esbuild = require("esbuild");
-
-esbuild.build({
-  entryPoints: ["src/scripts.js"],
-  bundle: true,
-  outfile: "dist/bundle.js",
-  platform: "browser",
-}).catch(() => process.exit(1));
